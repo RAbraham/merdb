bf9488a5c4a97864fbba64d31eafd769cbba0ddd
Rajiv Abraham <rajiv.abraham@gmail.com>
2023-08-28 06:15:35 -0400

    add error test

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -93,6 +93,57 @@ def test_pipe():
     assert_str(result, exp_df)
 
 
+def test_common_parent_error():
+    # from merdb.api.tacit_interactive import *
+    # import pandas as pd
+
+    # def add(col): return col.sum()
+    #
+    #
+    # input_df = pd.DataFrame([
+    #     ["toronto", 10],
+    #     ["montreal", 20],
+    #     ["toronto", 30],
+    #     ["halifax", 5]
+    # ], columns=["location", "population"])
+    #
+    # # people = table(input_df)
+    # # total = agg(people, add, "population")
+    #
+    # r = t(input_df) | agg(lambda c: add(c), "population")
+    #
+    # print(r)
+    #
+
+    data = {
+        'price': [100, 50, 150],
+        'volume': [2, 1, 1]
+    }
+
+    trades_df = pd.DataFrame(data)
+
+    print(trades_df)
+
+    weighted_avg_price = (trades_df['price'] * trades_df['volume']).sum() / trades_df['volume'].sum()
+    print('Weighted average price by Pandas:', weighted_avg_price)
+
+    tt = t(trades_df)
+    mul = tt | map(lambda r: r['price'] * r['volume'], "mul")
+    mul_sum = mul | agg(lambda c: c.sum(), "mul", "mul_sum")
+    suma = tt | agg(lambda c: c.sum(), "volume", "sum")
+    zipped = mul_sum | join_inner(suma)
+    result = zipped | map(lambda r: r["mul_sum"] / r["mul"], "total")
+
+    print("Mul")
+    print(mul)
+    print("Mul Sum")
+    print(mul_sum)
+    print("Suma")
+    print(suma)
+    print("zipped")
+    print(zipped)
+    # print("r")
+    #
 
 def is_senior(r: Row[C.age: int, C.name: str]) -> bool:
     return r['age'] > 35
